c6eb0b6569dc772d913c6275ac9fc5c7a1c7f59a
Yaremko Ivan <yaremich98@gmail.com>
2022-01-05 11:10:50 +0200

    Select examples

--- select.sql
+++ select.sql
@@ -14,4 +14,12 @@ WHERE "id"%2=0 AND "is_male"=false;
 
 SELECT * 
 FROM "users"
-WHERE "first_name"='Julia';
+WHERE "first_name"='Ivan';
+
+SELECT * 
+FROM "users"
+WHERE "first_name" in ('Julia', 'Lia', 'Anna', 'Elon');
+
+SELECT * 
+FROM "users"
+WHERE "id" BETWEEN 1400 AND 1700;
